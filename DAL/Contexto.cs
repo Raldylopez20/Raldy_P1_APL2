0433cea6d7e5a790c2ec68117da656bd6bff59ca
Raldy Steven Lopez Gilbert <raldy_lopez@ucne.edu.do>
2021-06-02 18:46:32 -0400

    Context agregada

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using Raldy_P1_APL2.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace Raldy_P1_APL2.DAL
 {
-    public class Contexto
+    public class Contexto : DbContext
     {
 
+        public DbSet<Productos> Productos { get; set; }
 
-
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = Data/Productos.db");
+        }
     }
 }
